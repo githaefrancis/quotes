c026c934d177f27e6c7bc839b7fb31b2ffae3c18
Francis Githae <francis.githae@student.moringaschool.com>
2021-12-29 22:24:12 +0300

    add initial quotes

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -9,7 +9,8 @@ import { Quote } from '../quote';
 export class QuoteComponent implements OnInit {
 
   quotes:Quote[]=[
-    new Quote(1,)
+    new Quote(1,"It always seems impossible until it's done.","Nelson Mandela"),
+    new Quote(2,"Change will not come if we wait for some other person, or if we wait for some other time. We are the ones we've been waiting for. We are the change that we seek.","Barack Obama")
   ]
   constructor() { }
 
