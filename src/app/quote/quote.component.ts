4fb53f82808fdc7b60a5d065d8db2eae96bb97da
Francis Githae <francis.githae@student.moringaschool.com>
2021-12-30 07:36:08 +0300

    add upvote and downvote buttons

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Quote } from '../quote';
+import { faCaretDown, faCaretUp, faCoffee } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-quote',
@@ -7,10 +8,13 @@ import { Quote } from '../quote';
   styleUrls: ['./quote.component.css']
 })
 export class QuoteComponent implements OnInit {
-
+  // faCoffee=faCoffee;
+  faCaretUp=faCaretUp;
+  faCaretDown=faCaretDown;
   quotes:Quote[]=[
     new Quote(1,"It always seems impossible until it's done.","Nelson Mandela"),
-    new Quote(2,"Change will not come if we wait for some other person, or if we wait for some other time. We are the ones we've been waiting for. We are the change that we seek.","Barack Obama")
+    new Quote(2,"Change will not come if we wait for some other person, or if we wait for some other time. We are the ones we've been waiting for. We are the change that we seek.","Barack Obama"),
+    new Quote(3,"We're here to put a dent in the universe. Otherwise why else even be here?","Steve Jobs")
   ]
   constructor() { }
 
@@ -18,3 +22,4 @@ export class QuoteComponent implements OnInit {
   }
 
 }
+
